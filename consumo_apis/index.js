f2d3886f3202a1652e2dc183818fac30fe7a353f
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-06-16 18:32:25 -0300

    parametros nas requisições

--- consumo_apis/index.js
+++ consumo_apis/index.js
@@ -3,27 +3,37 @@ const app = express()
 
 app.listen('3000')
 
-// get
-app.route('/').get((req, res) => res.send(author))
+// // get
+// app.route('/').get((req, res) => res.send(author))
 
-// //middleware /post
-app.use(express.json())
-// post
-app.route('/').post((req, res) => res.send(req.body))
+// // //middleware /post
+// app.use(express.json())
+// // post
+// app.route('/').post((req, res) => res.send(req.body))
 
-//put - editar informações
-app.use(express.json())
+// //put - editar informações
+// app.use(express.json())
 
-let author = "Mattheus"
+// let author = "Mattheus"
 
-app.route('/').put((req, res) => {
-    author = req.body.author
-    res.send(author)
-})
+// app.route('/').put((req, res) => {
+//     author = req.body.author
+//     res.send(author)
+// })
 
-//delete
+// //delete
 
-app.route('/:identificador').delete((req, res) => {
+// app.route('/:identificador').delete((req, res) => {
+//     res.send(req.params.identificador)
+// })
 
-    res.send(req.params.identificador)
-})
+// body params 
+// app.use(express.json())
+// app.route('/').post((req, res) => {
+//     const { nome, cidade } = req.body
+//     res.send(`meu nome é ${nome} e minha cidade é ${cidade}`);
+// })
+
+app.route('/:').get((req, res) => res.send("oi"))
+app.route('/:variavel').get((req, res) => res.send(req.params.variavel))
+
